daaf2e5639193fddc1a32590e800f01ea348bfdb
Archucha <enzoduran828@gmail.com>
2025-06-18 22:16:29 -0300

    Creo q termine

--- Tp 18-06/Participante.cs
+++ Tp 18-06/Participante.cs
@@ -10,11 +10,9 @@ namespace Tp_18_06
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public string MostrarNombreCompleto(string nombrecompleto)
-        {
-            Participante p = new Participante();
-            nombrecompleto = p.Nombre + " " + p.Apellido;
-            return nombrecompleto;
+        public void MostrarNombreCompleto()
+        { 
+            Console.WriteLine(Nombre + " " + Apellido);
         }
 
     }
--- Tp 18-06/Program.cs
+++ Tp 18-06/Program.cs
@@ -4,6 +4,7 @@ using Tp_18_06;
 internal class Program
 {
     public List<Evento> eventos = new List<Evento>();
+    public Participante p = new Participante();
     public void CrearEvento()
     {
         Console.WriteLine("Ingrese que tipo de evento es");
@@ -55,12 +56,46 @@ internal class Program
         {
             if (evento == eve.Nombre)
             {
-                eve.ObtenerDescripcion(); 
+                eve.ObtenerDescripcion();
+                foreach (Participante participante in eve.Participantes)
+                {
+                    participante.MostrarNombreCompleto();
+                }
             }
         }
     }
-    private static void Main(string[] args)
+    public void InscribirParticipante(Participante par)
     {
-
+        Console.WriteLine("Ingrese el nombre");
+        par.Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese el apellido");
+        par.Apellido = Console.ReadLine();
+    }
+    public void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese que desea hacer");
+        Console.WriteLine("1: Crear evento");
+        Console.WriteLine("2: Mostrar lista de eventos");
+        Console.WriteLine("3: Buscar eventos");
+        Console.WriteLine("4: Inscribir participantes");
+        int sel = int.Parse(Console.ReadLine());
+        switch(sel)
+        {
+            case 1:
+                CrearEvento();
+                break;
+            case 2:
+                ListarEventos();
+                break;
+            case 3:
+                BuscarEvento();
+                break;
+            case 4:
+                InscribirParticipante(p);
+                break;
+            default: 
+                Console.WriteLine("Ingrese una opcion valida");
+                break;
+        }
     }
 }
